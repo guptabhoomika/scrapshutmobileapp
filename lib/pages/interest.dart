33d22685fa9292b0ab3130510eea989adc5ddc54
guptabhoomika <43852276+guptabhoomika@users.noreply.github.com>
2020-05-05 21:29:31 +0530

    content

--- lib/pages/interest.dart
+++ lib/pages/interest.dart
@@ -199,11 +199,18 @@ class _InterestState extends State<Interest> {
         ListView(
           
           children: <Widget>[
-            Container(
-              height: 100,
-              width: 100,
-              
-            ),
+             Container(
+    
+            child: Text(
+                " KiRa dataset's powered by ScrapShut\n Opensource DataSet's for every aspiring technocrat \n The data you give = The data you get \n Submit your tags \ E.g: ML,AI,OpenCV,Marketing \n Based on your Interest\n visit \"Developers.scrapshut.com\" ",
+          textAlign: TextAlign.center,
+    
+        ),
+            padding: new EdgeInsets.all(30.0),
+              height: 200,
+              width: 200,
+             
+),
             Padding(
               padding: const EdgeInsets.only(top: 50,left: 80,right: 20),
               child: Container(
